a87439ea2b3e21e36ba53b00a5743408e531f411
Rowan Christmas <rowanxmas@gmail.com>
2026-05-14 10:32:51 -0700

    Add xmas-sbx-ui cask

---
+++ Casks/xmas-sbx-ui.rb
@@ -0,0 +1,17 @@
+cask "xmas-sbx-ui" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/docker/xmas-sbx-ui/releases/download/v#{version}/sbx-ui_#{version}_aarch64.dmg"
+  name "Docker Sandboxes"
+  desc "GUI for Docker Sandboxes (sbx)"
+  homepage "https://github.com/docker/xmas-sbx-ui"
+
+  app "sbx-ui.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.xmas.sbx-ui",
+    "~/Library/Caches/com.xmas.sbx-ui",
+    "~/Library/Preferences/com.xmas.sbx-ui.plist",
+  ]
+end
