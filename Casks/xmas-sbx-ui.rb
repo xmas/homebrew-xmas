c99b8b32f954f54f18921b5c0e6ea022dd14227c
Rowan Christmas <rowanxmas@gmail.com>
2026-05-14 15:23:25 -0700

    Point cask at public xmas/homebrew-xmas release for v0.3.0

--- Casks/xmas-sbx-ui.rb
+++ Casks/xmas-sbx-ui.rb
@@ -2,10 +2,10 @@ cask "xmas-sbx-ui" do
   version "0.3.0"
   sha256 "d01644c23f3094247047f91a6347d92b60195e6c398f448bce9338259eb34217"
 
-  url "https://github.com/docker/xmas-sbx-ui/releases/download/v#{version}/sbx-ui_#{version}_aarch64.dmg"
+  url "https://github.com/xmas/homebrew-xmas/releases/download/v#{version}/sbx-ui_#{version}_aarch64.dmg"
   name "Docker Sandboxes"
   desc "GUI for Docker Sandboxes (sbx)"
-  homepage "https://github.com/docker/xmas-sbx-ui"
+  homepage "https://github.com/xmas/homebrew-xmas"
 
   app "sbx-ui.app"
 
